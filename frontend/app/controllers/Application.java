7e76e614daee6f375f906d43d63a56738909b8da
Reto Bachmann-Gmür <reto@apache.org>
2011-09-30 10:54:17 +0200

    binaries don't belong into scource control, have to find out about play-maven integrartion

--- frontend/app/controllers/Application.java
+++ frontend/app/controllers/Application.java
@@ -5,12 +5,17 @@ import play.mvc.*;
 
 import java.util.*;
 
+import ch.unibe.ese.calendar.CalendarEvent;
+
 import models.*;
 
 public class Application extends Controller {
 
     public static void index() {
-        render();
+    	CalendarEvent event = new CalendarEvent(new Date(500), 
+    			new Date(1500), "an important second", true);
+        final String token = "a secret token";
+		render(token, event);
     }
 
 }
