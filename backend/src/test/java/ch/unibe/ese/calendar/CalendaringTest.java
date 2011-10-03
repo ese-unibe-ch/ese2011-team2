8ab4307aa69abf4ba7351e43b760b21da07be0ad
lyriael <lyriael@gmx.net>
2011-10-03 19:45:25 +0200

    Added test for authorisations

--- backend/src/test/java/ch/unibe/ese/calendar/CalendaringTest.java
+++ backend/src/test/java/ch/unibe/ese/calendar/CalendaringTest.java
@@ -273,4 +273,27 @@ public class CalendaringTest {
 			throw e.getCause();
 		}
 	}
+	
+	@Test
+	public void anotherTest() {
+	        UserManager um = UserManager.getIsntance();
+	        final CalendarManager cm = CalendarManager.getInstance();
+	        
+	        Policy.setPolicy(new CalendarPolicy());
+	        User aaron = um.createUser("aaron", "ese");
+	       
+	        Subject.doAs(aaron.getSubject(), new PrivilegedAction<Object>() {
+	            @Override
+	            public Object run() {
+	                Calendar aaroncal = cm.createCalendar("Aarons Kalender");
+	                java.util.Calendar juc = java.util.Calendar.getInstance();
+	                juc.set(2011, 11, 23, 20, 15);
+	                Date start = juc.getTime();
+	                juc.set(2011, 11, 23, 23, 00);
+	                Date end = juc.getTime();
+	                aaroncal.addEvent(new CalendarEvent(start, end, "Toller Film", true));
+	                return null;
+	            }
+	        });
+	}
 }
--- frontend/app/jobs/Bootstrap.java
+++ frontend/app/jobs/Bootstrap.java
@@ -1,4 +1,6 @@
 package jobs;
+import java.security.Policy;
+import java.security.PrivilegedAction;
 import java.util.Date;
 
 import javax.security.auth.Subject;
@@ -8,6 +10,7 @@ import ch.unibe.ese.calendar.CalendarEvent;
 import ch.unibe.ese.calendar.CalendarManager;
 import ch.unibe.ese.calendar.User;
 import ch.unibe.ese.calendar.UserManager;
+import ch.unibe.ese.calendar.security.CalendarPolicy;
 import play.jobs.*;
  
 @OnApplicationStart
@@ -17,16 +20,26 @@ public class Bootstrap extends Job {
 	
     public void doJob() {
         UserManager um = UserManager.getIsntance();
-        CalendarManager cm = CalendarManager.getInstance();
+        final CalendarManager cm = CalendarManager.getInstance();
         
+        Policy.setPolicy(new CalendarPolicy());
         User aaron = um.createUser("aaron", "ese");
-        Calendar aaroncal = cm.createCalendar("Aarons Kalender");
-        java.util.Calendar juc = java.util.Calendar.getInstance();
-        juc.set(2011, 11, 23, 20, 15);
-        Date start = juc.getTime();
-        juc.set(2011, 11, 23, 23, 00);
-        Date end = juc.getTime();
-        aaroncal.addEvent(new CalendarEvent(start, end, "Toller Film", true));
+       
+        Subject.doAs(aaron.getSubject(), new PrivilegedAction<Object>() {
+            @Override
+            public Object run() {
+                Calendar aaroncal = cm.createCalendar("Aarons Kalender");
+                java.util.Calendar juc = java.util.Calendar.getInstance();
+                juc.set(2011, 11, 23, 20, 15);
+                Date start = juc.getTime();
+                juc.set(2011, 11, 23, 23, 00);
+                Date end = juc.getTime();
+                aaroncal.addEvent(new CalendarEvent(start, end, "Toller Film", true));
+                return null;
+            }
+        });
+        
+        
         
         User judith = um.createUser("judith", "ese");
         User reto = um.createUser("reto", "ese");
