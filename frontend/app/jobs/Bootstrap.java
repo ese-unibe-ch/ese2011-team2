b032fcff20f42a867110c52eeda373c268529846
lyriael <lyriael@gmx.net>
2011-10-03 10:08:08 +0200

    Datebase extension
    minor changes in Bootstrap.java to be able to list a calendar with Events.

--- frontend/app/jobs/Bootstrap.java
+++ frontend/app/jobs/Bootstrap.java
@@ -1,14 +1,38 @@
 package jobs;
+import java.util.Date;
+
+import javax.security.auth.Subject;
+
+import ch.unibe.ese.calendar.Calendar;
+import ch.unibe.ese.calendar.CalendarEvent;
+import ch.unibe.ese.calendar.CalendarManager;
+import ch.unibe.ese.calendar.User;
 import ch.unibe.ese.calendar.UserManager;
 import play.jobs.*;
  
 @OnApplicationStart
 public class Bootstrap extends Job {
     
+	
+	
     public void doJob() {
         UserManager um = UserManager.getIsntance();
-        um.createUser("aaron", "ese");
+        CalendarManager cm = CalendarManager.getInstance();
+        
+        User aaron = um.createUser("aaron", "ese");
+        Calendar aaroncal = cm.createCalendar("Aarons Kalender");
+        java.util.Calendar juc = java.util.Calendar.getInstance();
+        juc.set(2011, 11, 23, 20, 15);
+        Date start = juc.getTime();
+        juc.set(2011, 11, 23, 23, 00);
+        Date end = juc.getTime();
+        aaroncal.addEvent(new CalendarEvent(start, end, "Toller Film", true));
+        
+        User judith = um.createUser("judith", "ese");
+        User reto = um.createUser("reto", "ese");
         //TODO add a default calendar
+        
+        
     }
     
 }
