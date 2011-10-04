9b29adb719372713aa7ae1efff6a7cac682e34b9
lyriael <lyriael@gmx.net>
2011-10-04 11:40:41 +0200

    Changes to view CalendarEvents
    Links and templates stuff

--- frontend/app/controllers/Application.java
+++ frontend/app/controllers/Application.java
@@ -1,7 +1,11 @@
 package controllers;
 
+import java.security.PrivilegedAction;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.Set;
+
+import javax.security.auth.Subject;
 
 import play.*;
 import play.mvc.*;
@@ -12,6 +16,7 @@ import ch.unibe.ese.calendar.CalendarEvent;
 import ch.unibe.ese.calendar.CalendarManager;
 import ch.unibe.ese.calendar.User;
 import ch.unibe.ese.calendar.UserManager;
+import ch.unibe.ese.calendar.exceptions.CalendarAlreayExistsException;
 
 import models.*;
 
@@ -23,14 +28,29 @@ public class Application extends Controller {
     	User user = UserManager.getIsntance().getUserByName(userName);
     	CalendarManager calendarManager = CalendarManager.getInstance();
     	Set<Calendar> userCalendars = calendarManager.getCalendarsOf(user);
-    	   	
+    	
     	CalendarEvent event = new CalendarEvent(new Date(500), 
     			new Date(1500), "an important second", true);
     	
     	
         final String token = "You ("+user+") own: "+userCalendars.size()+" calendars";
-        //TODO return user calendars
 		render(token, event, userCalendars);
     }
+
+    public static void calendar(String name) {
+    	System.out.println("name: "+name);
+    	String userName = Security.connected();
+    	User user = UserManager.getIsntance().getUserByName(userName);
+    	CalendarManager calendarManager = CalendarManager.getInstance();
+    	final Calendar calendar = calendarManager.getCalendar(name);
+    	Iterator<CalendarEvent> iterator = Subject.doAs(user.getSubject(), new PrivilegedAction<Iterator<CalendarEvent>>() {
+			@Override
+			public Iterator<CalendarEvent> run() {		
+				return calendar.iterate(new Date(0));
+			}
+		});
+    	render(iterator);  	
+    }
+    
 
 }
