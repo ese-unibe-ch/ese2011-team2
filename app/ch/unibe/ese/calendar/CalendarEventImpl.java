c2f6fad8b811714a0f6f4664252434d2fa6c6954
Reto Bachmann-Gmür <reto@apache.org>
2011-11-01 20:51:14 +0100

    added forgotten files

---
+++ app/ch/unibe/ese/calendar/CalendarEventImpl.java
@@ -0,0 +1,21 @@
+package ch.unibe.ese.calendar;
+
+import java.util.Date;
+
+class CalendarEventImpl extends CalendarEntry implements CalendarEvent {
+	
+	protected boolean isASerie;
+	
+	CalendarEventImpl(Date start, Date end, String name, String visibility, EseCalendar calendar, String description) {
+		super(start, end, name, visibility, calendar, description);
+		isASerie = false;
+		
+	}
+	/* (non-Javadoc)
+	 * @see ch.unibe.ese.calendar.CalendarEvent#isASerie()
+	 */
+	public boolean isASerie(){
+		return isASerie;
+	}
+	
+}
