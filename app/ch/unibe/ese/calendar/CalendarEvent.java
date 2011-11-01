bd4c0cf945950ef249bcc421215752f2b10ce708
Reto Bachmann-Gmür <reto@apache.org>
2011-11-01 19:25:25 +0100

    reduced visibilty of CalendarEvent constructor to package

--- app/ch/unibe/ese/calendar/CalendarEvent.java
+++ app/ch/unibe/ese/calendar/CalendarEvent.java
@@ -6,7 +6,7 @@ public class CalendarEvent extends CalendarEntry {
 	
 	protected boolean isASerie;
 	
-	public CalendarEvent(Date start, Date end, String name, String visibility, EseCalendar calendar, String description) {
+	CalendarEvent(Date start, Date end, String name, String visibility, EseCalendar calendar, String description) {
 		super(start, end, name, visibility, calendar, description);
 		isASerie = false;
 		
