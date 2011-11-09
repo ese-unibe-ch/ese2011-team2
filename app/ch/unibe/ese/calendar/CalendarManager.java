c181f3e3e0d9523a88a2cc7fea27d8ca0d0ddb63
Reto Bachmann-Gmür <reto@apache.org>
2011-11-09 18:53:48 +0100

    Added UnionCalendar with possibility to get the union calendar of a user  (no tests yet)

--- app/ch/unibe/ese/calendar/CalendarManager.java
+++ app/ch/unibe/ese/calendar/CalendarManager.java
@@ -11,16 +11,57 @@ public abstract class CalendarManager {
 
 	private static CalendarManager instance = new CalendarManagerImpl();
 
+	/**
+	 * permanently deletes all calendars managed my this CalendarManager
+	 * @param user the user requesting the operation
+	 */
 	public abstract void purge(User user);
 
+	/**
+	 * Get the calendars own by a user. They are sorted by their name.
+	 * 
+	 * @param user the user for which the calendars are requested
+	 * @return the calendars of user
+	 */
 	public abstract SortedSet<EseCalendar> getCalendarsOf(User user);
+	
+	/**
+	 * Get a Union of all Calendar of the user. If and how operation modifying
+	 * the calendar are supported is not defined.
+	 * 
+	 * @param user the user for which the calendars are requested
+	 * @return the union calendar of user
+	 */
+	public abstract EseCalendar getUnionCalendarOf(User user);
 
+
+	/**
+	 * Get a calendar with the specified name
+	 * 
+	 * @param name the name of the calendar
+	 * @return the existing calendar
+	 * @throws NoSuchCalendarException if no calendar with that name exists
+	 */
 	public abstract EseCalendar getCalendar(String name)
 			throws NoSuchCalendarException;
 
+	/**
+	 * Create a calendar with the specified name. This method must be invoked as a Subject with exactly
+	 * one CalendarPriciple, i.e. as the Subject returned by <code>User.getSubject</code>.
+	 * 
+	 * @param name the name of the calendar
+	 * @return the newly created calendar
+	 * @throws CalendarAlreadyExistsException if a calendar with that name already exists
+	 */
 	public abstract EseCalendar createCalendar(User user, String name)
 			throws CalendarAlreadyExistsException;
 	
+	/**
+	 * Remove a calendar with the specified name.
+	 * 
+	 * @param name the name of the calendar
+	 * @throws NoSuchCalendarException if there is no calendar with that name
+	 */
 	public abstract void removeCalendar(String name)
 			throws NoSuchCalendarException;
 
---
+++ app/ch/unibe/ese/calendar/util/UnionCalendar.java
@@ -0,0 +1,104 @@
+package ch.unibe.ese.calendar.util;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import ch.unibe.ese.calendar.CalendarEvent;
+import ch.unibe.ese.calendar.EseCalendar;
+import ch.unibe.ese.calendar.EventSeries;
+import ch.unibe.ese.calendar.EventSeries.Repetition;
+import ch.unibe.ese.calendar.User;
+import ch.unibe.ese.calendar.Visibility;
+import ch.unibe.ese.calendar.exceptions.EventNotFoundException;
+
+/**
+ * 
+ * A union of multiple calendar
+ * 
+ * Method calls changing the Calendar only affect one main-calendar, all other operation are delegated to 
+ * all members of the union
+ *
+ */
+public class UnionCalendar extends AbstractCalendar {
+
+	private EseCalendar mainCalendar;
+	private EseCalendar[] otherCalendars;
+
+	/**
+	 * 
+	 * @param mainCalendar the calendar against which operation modifying the calendar are perfmormed
+	 * @param otherCalendars the other calendars of union
+	 */
+	public UnionCalendar(EseCalendar mainCalendar, EseCalendar... otherCalendars) {
+		this.mainCalendar = mainCalendar;
+		this.otherCalendars = otherCalendars;
+	}
+	
+	@Override
+	public Iterator<CalendarEvent> iterate(User user, Date start) {
+		List<Iterator<CalendarEvent>> baseIterators = new ArrayList<Iterator<CalendarEvent>>();
+		baseIterators.add(mainCalendar.iterate(user, start));
+		for(EseCalendar other: otherCalendars) {
+			baseIterators.add(other.iterate(user, start));
+		}
+		return new EventIteratorMerger(baseIterators);
+	}
+
+	@Override
+	public CalendarEvent getEventById(User user, String id) {
+		for(EseCalendar other: otherCalendars) {
+			try {
+				return other.getEventById(user, id);
+			} catch (EventNotFoundException e) {
+				
+			}
+		}
+		return mainCalendar.getEventById(user, id);
+	}
+
+	@Override
+	public CalendarEvent removeEvent(User user, String id) {
+		return mainCalendar.removeEvent(user, id);
+	}
+
+	@Override
+	public EventSeries removeEventSeries(User user, String id) {
+		return mainCalendar.removeEventSeries(user, id);
+	}
+
+	@Override
+	public EventSeries addEventSeries(User user, Date start, Date end,
+			String eventName, Visibility visibility, Repetition repetition,
+			String description) {
+		return mainCalendar.addEventSeries(user, start, end, eventName, visibility, repetition, description);
+	}
+
+	@Override
+	public CalendarEvent addEvent(User user, Date start, Date end,
+			String eventName, Visibility visibility, String description) {
+		return mainCalendar.addEvent(user, start, end, eventName, visibility, description);
+	}
+
+	/**
+	 * @return the owner of the main calendar
+	 */
+	@Override
+	public User getOwner() {
+		return mainCalendar.getOwner();
+	}
+
+	@Override
+	public String getName() {
+		Set<String> baseNames = new HashSet<String>();
+		baseNames.add(mainCalendar.getName());
+		for(EseCalendar other: otherCalendars) {
+			baseNames.add(other.getName());
+		}
+		return "Union of "+baseNames.toString();
+	}
+
+}
