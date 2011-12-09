9c58069fc0d759036b8872a36a9e8f83191e1113
panmari <smmuzi@gmail.com>
2011-12-09 10:02:47 +0100

    Fixed very old bug (initially found by bteuscher)
    Events couldn't be edited to "Contacts only" visibility.
    Also handled the enum-translation issue by removing the string representation in the java code and adding it to the messages file.

--- app/ch/unibe/ese/calendar/Visibility.java
+++ app/ch/unibe/ese/calendar/Visibility.java
@@ -10,30 +10,19 @@ public enum Visibility {
 	/**
 	 * The event is private, only a user with PrivilegedAccess to the calendar can see it.
 	 */
-	PRIVATE("private"),
+	PRIVATE,
 	/**
 	 * The event is public, everybody can see it.
 	 */
-	PUBLIC("public"),
+	PUBLIC,
 	/**
 	 * Without privileged calendar access one can only seen the time span of an event 
 	 * to be busy but no other event details.
 	 */
-	BUSY("show as busy"),
+	BUSY,
 	/**
 	 * The event is visible for users that are in myContacts. All other users see the 
 	 * event as busy.
 	 */
-	CONTACTSONLY("contacts only");
-	
-	private String name;
-	
-	private Visibility(String name) {
-		this.name = name;
-	}
-	
-	public String getName() {
-		return name;
-	}
-
+	CONTACTSONLY;
 }
