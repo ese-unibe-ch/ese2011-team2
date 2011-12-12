80ed2393e0ee3ee22803dd2e4a493c1e5ef672f3
Thomas <thomas_riedo@hotmail.com>
2011-12-12 15:39:16 +0100

    change language on user page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 
 import javax.activity.InvalidActivityException;
 
+import play.i18n.Lang;
 import play.mvc.Controller;
 import play.mvc.With;
 import ch.unibe.ese.calendar.CalendarEvent;
@@ -198,7 +199,18 @@ public class Application extends Controller {
 				currentUserName);
 		SortedSet<EseCalendar> calendars = CalendarManager.getInstance()
 				.getCalendarsOf(connectedUser);
-		render(connectedUser, calendars);
+		String lang = Lang.get();
+		String[] languages = new String[4];
+		languages[0] = "en";
+		languages[1] = "de";
+		languages[2] = "fr";
+		languages[3] = "ja";
+		render(connectedUser, calendars, lang,languages);
+	}
+	
+	public static void changeLanguage(String language){
+		Lang.change(language);
+		user();
 	}
 	
 	public static void addToContacts(String name) {
